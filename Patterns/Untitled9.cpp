cdda8094f22445c2ccf8c9caa65d2c138af68035
Tarunverma504 <vermatarun4305@gmail.com>
2020-02-02 19:44:15 +0530

    created

---
+++ Patterns/Untitled9.cpp
@@ -0,0 +1,29 @@
+/*
+    5
+   45
+  345
+ 2345
+12345
+*/
+#include<stdio.h>
+int main()
+{
+	int i,j,x;
+    for(i=5;i>=1;i--)
+	{
+		x=i;
+		for(j=1;j>=5;j++)
+		{
+			if(j>=i)
+			{
+				printf("%d",x--);
+			}
+			else if(j<i)
+			printf(" ");
+			
+		}
+		
+	printf("\n");
+}
+return 0;
+}
